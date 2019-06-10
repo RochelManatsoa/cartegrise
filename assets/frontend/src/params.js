11bb58ac3d41008f272f10e7f05417414c6c386e
patrick_xtn <rapaelec@gmail.com>
2019-06-10 18:22:11 +0300

    add config for all environement

--- assets/frontend/src/params.js
+++ assets/frontend/src/params.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 const ENTRYPOINT = 'https://127.0.0.1:8000/api';
+// const ENTRYPOINT = 'http://dev2.cgofficiel.fr/api'; // DEV
+// const ENTRYPOINT = 'http://www.cgofficiel.fr/api';  // PROD
 
 export default {ENTRYPOINT};
