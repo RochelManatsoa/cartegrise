4a011cb13821bc3bd219e0491a22274361a27b85
root <root@vps675948.ovh.net>
2019-06-11 14:25:32 +0200

    change entrypoint to https

--- assets/frontend/src/params.js
+++ assets/frontend/src/params.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 //const ENTRYPOINT = 'https://127.0.0.1:8000/api';
 // const ENTRYPOINT = 'http://dev2.cgofficiel.fr/api'; // DEV
-const ENTRYPOINT = 'http://www.cgofficiel.fr/api';  // PROD
+const ENTRYPOINT = 'https://www.cgofficiel.fr/api';  // PROD
 
 export default {ENTRYPOINT};
