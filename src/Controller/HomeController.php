5b987c4954c4f26a754d971a587e4bf2dd10366e
patrick_xtn <rapaelec@gmail.com>
2019-06-14 20:35:54 +0300

    add contact us and delete unused code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,27 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Demande;
-use App\Entity\Commande;
-use App\Entity\Taxes;
-use App\Entity\TypeDemande;
-use App\Form\DemandeType;
-use App\Form\CommandeType;
-use App\Repository\CommandeRepository;
-use App\Repository\TaxesRepository;
-use App\Repository\TarifsPrestationsRepository;
-use App\Repository\DemandeRepository;
-use App\Repository\TypeDemandeRepository;
+use App\Entity\{Demande, ContactUs, Commande, Taxes, TypeDemande};
+use App\Form\{DemandeType, CommandeType, ContactUsType};
+use App\Repository\{CommandeRepository, TaxesRepository, TarifsPrestationsRepository, DemandeRepository, TypeDemandeRepository};
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Manager\SessionManager;
 use App\Services\Tms\TmsClient;
-use App\Manager\CommandeManager;
-use App\Manager\TaxesManager;
-use App\Manager\CarInfoManager;
+use App\Manager\{SessionManager, CommandeManager, TaxesManager, CarInfoManager};
 
 
 class HomeController extends AbstractController
@@ -134,41 +123,11 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @Route("/commande", name="commande")
-     */
-    public function demande(Request $request)
-    {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $user = $this->getUser();
-            $client = $user->getClient();
-            $genre = $client->getClientGenre();
-
-            return $this->render('home/demande.html.twig', [
-                'genre' => $genre,
-                'client' => $client,
-            ]);
-        }
-
-        return $this->render('home/demande.html.twig');
-    }
-
-    /**
      * @Route("/CommentCaMarche", name="CommentCaMarche")
      */
     public function CommentCaMarche()
     {
-            if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-                    $user = $this->getUser();
-                    $client = $user->getClient();
-                    $genre = $client->getClientGenre();
-
-                    return $this->render('home/CommentCaMarche.html.twig', [
-                            'genre' => $genre,
-                            'client' => $client,
-                    ]);
-            }
-
-            return $this->render('home/CommentCaMarche.html.twig');
+        return $this->render('home/CommentCaMarche.html.twig');
     }
 
     /**
@@ -176,17 +135,6 @@ class HomeController extends AbstractController
      */
     public function faq()
     {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $user = $this->getUser();
-            $client = $user->getClient();
-            $genre = $client->getClientGenre();
-
-            return $this->render('home/faq.html.twig', [
-                'genre' => $genre,
-                'client' => $client,
-            ]);
-        }
-
         return $this->render('home/faq.html.twig');
     }
 
@@ -195,18 +143,7 @@ class HomeController extends AbstractController
 	 */
 	public function cgv()
 	{
-			if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-					$user = $this->getUser();
-					$client = $user->getClient();
-					$genre = $client->getClientGenre();
-
-					return $this->render('home/cgv.html.twig', [
-							'genre' => $genre,
-							'client' => $client,
-					]);
-            }
-
-			return $this->render('home/cgv.html.twig');
+		return $this->render('home/cgv.html.twig');
 	}
 
 	/**
@@ -214,147 +151,28 @@ class HomeController extends AbstractController
 	 */
 	public function retractation()
 	{
-			if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-					$user = $this->getUser();
-					$client = $user->getClient();
-					$genre = $client->getClientGenre();
-
-					return $this->render('home/retractation.html.twig', [
-							'genre' => $genre,
-							'client' => $client,
-					]);
-			}
-			return $this->render('home/retractation.html.twig');
+        return $this->render('home/retractation.html.twig');
 	}
 
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request)
     {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $user = $this->getUser();
-            $client = $user->getClient();
-            $genre = $client->getClientGenre();
+        $contactU = new ContactUs();
+        $form = $this->createForm(ContactUsType::class, $contactU);
+        $form->handleRequest($request);
 
-            return $this->render('home/contact.html.twig', [
-                'genre' => $genre,
-                'client' => $client,
-            ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($contactU);
+            // $entityManager->flush();
+
+            // return $this->redirectToRoute('contact_us_index');
         }
-        return $this->render('home/contact.html.twig');
-    }
 
-    /**
-     * @Route("/espace", name="espace")
-     */
-    public function index()
-    {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $user = $this->getUser();
-            $client = $user->getClient();
-            $genre = $client->getClientGenre();
-
-            return $this->render('home/index.html.twig', [
-                'genre' => $genre,
-                'client' => $client,
-            ]);
-        }
-        return $this->render('home/index.html.twig');
-    }
-	
-	/**
-    * @Route("/dc", name="dc")
-    */
-    public function dcdemande() {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-            $user = $this->getUser();
-            $client = $user->getClient();
-            $idclient = $client->getId();
-            $nom = $client->getClientNom();
-            $prenom = $client->getClientPrenom();
-            $genre = $client->getClientGenre();
-            $dateNaissance = $client->getClientDateNaissance();
-            $lieuNaissance = $client->getClientLieuNaissance();
-            $dpt = $client->getClientDptNaissance();
-            $pays = $client->getClientPaysNaissance();
-
-            $contact = $client->getClientContact();
-            $idcontact = $contact->getId();
-            $mobile = $contact->getContactTelmobile();
-            $mail = $user->getEmail();
-
-        return $this->render('demarche/dc.html.twig', [
-                'idclient' => $idclient,
-                'mail' => $mail,
-                'mobile' => $mobile,
-                'pays' => $pays,
-                'genre' => $genre,
-                'dateN' => $dateNaissance,
-                'lieuN' => $lieuNaissance,
-                'dptN' => $dpt,
-                'client' => $client
-        ]); 
-    }
-
-    /**
-    *@Route("/dc/checkout/{tmsId}", name="checkoutdc")
-    */
-    public function chekoutDC($tmsId) {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        $user = $this->getUser();
-        $client = $user->getClient();
-        $idclient = $client->getId();
-        $genre = $client->getClientGenre();
-        $demande = $this->getDoctrine()
-            ->getRepository(Demande::class)
-            ->findOneBy(['client' => $idclient, 'typeDemande' => 'DC', 'TmsIdDemande' => $tmsId]);
-            if ($demande == NULL) {
-                return $this->redirectToRoute('dc');
-            }
-            else{
-                $idDemande = $demande->getId();
-                return $this->render('demarche/checkout.html.twig',[
-                    'idDemande' => $idDemande,
-                    'tmsId' => $tmsId,
-                    'type' => 'DC',
-                    'genre' => $genre,
-                    'client' => $client,
-                ]);
-            }
-               
-    }
-
-    /**
-     *@Route("/tay", name="tay")
-     */
-    public function tay(CommandeManager $commandeManager)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $client = $this->getUser()->getClient();
-        $commande = $em->getRepository(Commande::class)->find(46);
-        $document = $commandeManager->editer($commande);
-
-        $decoded = \base64_decode($document->getRawData()->Document);
-        $file = 'CERFA.pdf';
-        $filefinal = file_put_contents($file, $decoded);
-        echo $file;die;
-    }
-    /**
-     *@Route("/mail_test", name="mail_test")
-     */
-    public function mail(\Swift_Mailer $mailer)
-    {
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('no-reply@cgofficiel.fr')
-            ->setTo('rapaelec@gmail.com')
-            ->setBody('mandeha')
-        ;
-
-        $mailer->send($message);
-
-        return new Response("vita");
+        return $this->render('home/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
