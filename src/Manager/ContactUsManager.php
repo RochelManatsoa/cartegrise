511b3a576521b043b88e9b3cdc1f9be95da3c6d7
patrick_xtn <rapaelec@gmail.com>
2019-06-28 10:47:21 +0300

    add register for conctactUs

---
+++ src/Manager/ContactUsManager.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * @Author: Patrick &lt;&lt; rapaelec@gmail.com &gt;&gt; 
+ * @Date: 2019-04-17 13:14:01 
+ * @Last Modified by: Patrick << rapaelec@gmail.com >>
+ * @Last Modified time: 2019-06-22 23:39:49
+ */
+namespace App\Manager;
+
+use App\Entity\ContactUs as Entity;
+use App\Repository\ContactUsRepository as Repository;
+use Doctrine\ORM\EntityManagerInterface as EntityManager;
+
+class ContactUsManager
+{
+    private $repository;
+    private $entityManager;
+    public function __construct(
+        Repository $repository,
+        EntityManager $entityManager
+    )
+    {
+        $this->repository = $repository;
+        $this->entityManager = $entityManager;
+    }
+
+    private function save(Entity $entity)
+    {
+        $this->EntityManager->persist($entity);
+        $this->EntityManager->flush();
+    }
+    
+}
