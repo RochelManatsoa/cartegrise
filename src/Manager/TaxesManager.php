cb5d7b2695d97113ee535207cdae0b45f2a81325
patrick_xtn <rapaelec@gmail.com>
2019-07-25 13:27:06 +0300

    debug carInfo

--- src/Manager/TaxesManager.php
+++ src/Manager/TaxesManager.php
@@ -4,7 +4,7 @@
  * @Author: stephan
  * @Date:   2019-04-15 12:27:51
  * @Last Modified by: Patrick << rapaelec@gmail.com >>
- * @Last Modified time: 2019-07-25 13:24:42
+ * @Last Modified time: 2019-07-25 13:27:02
  */
 
 namespace App\Manager;
@@ -59,7 +59,7 @@ class TaxesManager
         $otherINfo = $type === "ECG" ? ($typeDemarche === "DIVN" ? $value->Lot->Demarche->{$type}->Vehicule: $tmsResponse->Positive) : $tmsResponse->Positive;
         dd($otherINfo);
         $vin = $type === "ECG" ? null : $tmsResponse->Positive->VIN;
-        $dateMec = $type === "ECG" ? \DateTime::createFromFormat('d/m/Y', $otherINfo->DateMec) : \DateTime::createFromFormat('Y-m-d', $otherINfo->DateMec);
+        $dateMec = ($type === "ECG" && $typeDemarche === "DIVN") ? \DateTime::createFromFormat('d/m/Y', $otherINfo->DateMec) : \DateTime::createFromFormat('Y-m-d', $otherINfo->DateMec);
         $taxe->setVIN($vin)
             ->setCO2($otherINfo->CO2)
             ->setPuissance($otherINfo->PuissFisc)
