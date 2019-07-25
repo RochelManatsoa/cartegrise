ec655c6960c044b018a1d8d99ae4c452f13cd420
patrick_xtn <rapaelec@gmail.com>
2019-07-25 17:10:58 +0300

    duplicata demande

--- src/Manager/TaxesManager.php
+++ src/Manager/TaxesManager.php
@@ -4,7 +4,7 @@
  * @Author: stephan
  * @Date:   2019-04-15 12:27:51
  * @Last Modified by: Patrick << rapaelec@gmail.com >>
- * @Last Modified time: 2019-07-25 17:09:05
+ * @Last Modified time: 2019-07-25 17:10:54
  */
 
 namespace App\Manager;
@@ -63,7 +63,7 @@ class TaxesManager
         $otherINfo = $type === "ECG" ? (($typeDemarche === "DIVN") ? $value->Lot->Demarche->{$type}->Vehicule: $tmsResponse->Positive) : $tmsResponse->Positive;
         $vin = $type === "ECG" ? null : $tmsResponse->Positive->VIN;
         $dateMec = ($type === "ECG") ? ($typeDemarche === "DIVN")? \DateTime::createFromFormat('d/m/Y', $otherINfo->DateMEC) : \DateTime::createFromFormat('d/m/Y', $otherINfo->DateMec) : \DateTime::createFromFormat('Y-m-d', $otherINfo->DateMec);
-        
+        dd($otherINfo);
         $taxe->setVIN($vin)
             ->setCO2($otherINfo->CO2);
             if ($typeDemarche === "DIVN")
