b18ea0a14186417d307c173483ef78ec271dad26
Tinendrisitraka Diadema Tolotra RASOLONJATOVO <diadema.tolotra@etech.local>
2019-09-09 11:40:53 +0300

    ajustement responsive V2

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -66,7 +66,17 @@ function slugify(string) {
 $(document).ready(function () {
     $('.icon-mobile-menu').click(function(){
         $('#sidebar-container').toggleClass('show');
+        $('.new-header').toggleClass('show');
     });
+    var $paramPopup = $('#sidebar-container');
+    $(document).mouseup(function (e) {
+        if (!$paramPopup.is(e.target) && $paramPopup.has(e.target).length === 0){
+            if($('#sidebar-container').is(':visible')){
+                $('#sidebar-container').removeClass('show');
+                $('.new-header').removeClass('show');
+            }
+        }
+    });  
 });
 
 function valueTreatement(value) {
