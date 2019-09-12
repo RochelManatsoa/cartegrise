82f71ff7fe41854e1b4581e86037001e40a519ce
Rochel <nirinarochelmiandrisoa@gmail.com>
2019-09-12 19:09:45 +0200

    easyAdminBuldle, vichUploaderBundle  and ckeditorBundle

--- config/bundles.php
+++ config/bundles.php
@@ -31,4 +31,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
--- src/Entity/Blog/Article.php
+++ src/Entity/Blog/Article.php
@@ -7,9 +7,12 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\Blog\ArticleRepository")
+ * @Vich\Uploadable
  */
 class Article
 {
@@ -70,9 +73,52 @@ class Article
     private $categories;
 
     /**
-     * @ORM\OneToOne(targetEntity="App\Entity\Blog\Image", mappedBy="article", cascade={"persist", "remove"})
+     * @ORM\Column(type="string", length=255)
+     * @var string
      */
     private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="article_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    // ...
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Blog\Commentaire", mappedBy="article")
@@ -218,24 +264,6 @@ class Article
         return $this;
     }
 
-    public function getImage(): ?Image
-    {
-        return $this->image;
-    }
-
-    public function setImage(?Image $image): self
-    {
-        $this->image = $image;
-
-        // set (or unset) the owning side of the relation if necessary
-        $newArticle = $image === null ? null : $this;
-        if ($newArticle !== $image->getArticle()) {
-            $image->setArticle($newArticle);
-        }
-
-        return $this;
-    }
-
     /**
      * @return Collection|Commentaire[]
      */
@@ -263,6 +291,18 @@ class Article
                 $commentaire->setArticle(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
