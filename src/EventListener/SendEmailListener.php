932903d63ca8794ac4b2fdc2d9df67903b27c21b
rochel26 <nirinarochelmiandrisoa@gmail.com>
2019-10-31 08:13:16 +0100

    add SendMailListener

---
+++ src/EventListener/SendEmailListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventListener;
+
+use \Swift_Events_SendListener as base;
+
+class SendEmailListener implements base
+{
+    /**
+     * Invoked immediately before the Message is sent.
+     *
+     * @param \Swift_Events_SendEvent $evt
+     */
+    public function beforeSendPerformed(\Swift_Events_SendEvent $evt)
+    {
+        dump($evt);
+    }
+
+    /**
+     * Invoked immediately after the Message is sent.
+     *
+     * @param \Swift_Events_SendEvent $evt
+     */
+    public function sendPerformed(\Swift_Events_SendEvent $evt)
+    {
+        dd($evt);
+    }
+}
