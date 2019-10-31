a4a758fa364162b41d09f10053d077da7a121835
patrick_xtn <rapaelec@gmail.com>
2019-10-31 11:28:32 +0300

    email message traitment

--- src/EventListener/SendEmailListener.php
+++ src/EventListener/SendEmailListener.php
@@ -3,9 +3,14 @@
 namespace App\EventListener;
 
 use \Swift_Events_SendListener as base;
+use App\Manager\UserManager;
 
 class SendEmailListener implements base
 {
+    public function __construct(UserManager $userManager)
+    {
+        $this->userManager = $userManager;
+    }
     /**
      * Invoked immediately before the Message is sent.
      *
@@ -23,7 +28,30 @@ class SendEmailListener implements base
     public function sendPerformed(\Swift_Events_SendEvent $evt)
     {
         if (\Swift_Events_SendEvent::RESULT_SUCCESS === $evt->getResult()) {
-            dump($evt->getMessage());
+            $message = $evt->getMessage();
+            $body = $message->getBody();
+            $SwiftAdresseTo = $message->getHeaders()->get('To');
+            $SwiftAdresseFrom = $message->getHeaders()->get('From');
+            $subject = $message->getHeaders()->get('Subject')->getValue();
+            $froms = $this->getKeyArray($SwiftAdresseFrom->getNameAddresses());
+            $tos = $this->getKeyArray($SwiftAdresseTo->getNameAddresses());
+            dump($froms, $tos, $subject, $body);
+            foreach($tos as $to){
+                $user = $this->userManager->getUserByEmail($to);
+                dump($user);
+                // $emailHistory = new EmailHistory();
+                // $emailHistory->setSubject($subject)->setBody($body)->setFrom($froms);
+                // $user->addEmailHistory($emailHistory);
+                // $this->userManager->save($user);
+            }
         }
+    }
+
+    private function getKeyArray(array $array){
+        $result = [];
+        foreach($array as $key=>$value)
+            $result[] = $key;
+        
+        return $result;
     }
 }
