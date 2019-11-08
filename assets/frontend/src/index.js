28754990d1e231d47975e390731e550b82754d9a
patrick_xtn <rapaelec@gmail.com>
2019-11-08 23:07:22 +0300

    add css

--- assets/frontend/src/index.js
+++ assets/frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'react-notifications/lib/notifications.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'admin-lte/dist/css/AdminLTE.css';
+import 'react-notifications/lib/notifications.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
