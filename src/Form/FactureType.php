e50581016f870e446e15b8bcedeaa75e56187381
rochel26 <nirinarochelmiandrisoa@gmail.com>
2019-12-17 08:00:49 +0100

    factureType label

--- src/Form/FactureType.php
+++ src/Form/FactureType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Facture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FactureType extends AbstractType
@@ -12,11 +14,18 @@ class FactureType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('firstName')
-            ->add('label')
+            ->add('name', TextType::class,[
+                'attr'  => array( 'class' => 'text-uppercase' ),
+                'label' => 'label.nom.client'
+            ])
+            ->add('firstName', TextType::class,[
+                'label' => 'label.prenom.client'
+            ])
+            ->add('label', IntegerType::class,[
+                'label' => 'label.label'
+            ])
             ->add('adresse', AdresseType::class, [
-
+                'label' => 'label.clientAdresse'
             ])
         ;
     }
