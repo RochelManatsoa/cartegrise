970eb220d47662633ba4603cd0297f0f12a15112
rochel26 <nirinarochelmiandrisoa@gmail.com>
2020-01-24 15:50:09 +0100

    no tiret auto in samsung device

--- public/javascripts/tiretAuto.js
+++ public/javascripts/tiretAuto.js
@@ -1,13 +1,18 @@
 
 function valueTreatement(value) {
+	var ua = new UAParser();
+	var result = ua.getResult();
     var valueArray = value.split("");
-    if (valueArray.length === 7) {
-        return value.slice(0, 2) + "-" + value.slice(2, 5) + "-" + value.slice(5, 7);
-    } else if (valueArray.length === 9) {
+    if (result.device.vendor !== "Samsung"){
+        if (valueArray.length === 7) {
+            return value.slice(0, 2) + "-" + value.slice(2, 5) + "-" + value.slice(5, 7);
+        } else if (valueArray.length === 9) {
+            return value;
+        }
+        alert('Votre Numéro d\'immatriculation est incorrecte');
+    
+        return ""
+    } else {
         return value;
     }
-    alert('Votre Numéro d\'immatriculation est incorrecte');
-
-    return ""
-
 }
