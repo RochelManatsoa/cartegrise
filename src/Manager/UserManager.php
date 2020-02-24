c4fbeba1eedb6bf6d20befc3968b61b2fc312a1e
rochel26 <nirinarochelmiandrisoa@gmail.com>
2020-02-24 16:59:16 +0100

    debug 2 mails send

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -166,8 +166,7 @@ class UserManager
     public function sendEmailOnRegistration($user)
     {
         $template = 'email/register.mail.twig';
-        $this->mailManager->sendEmail($emails=[$user->getEmail()], $template, "Bienvenu sur CG Officiel - Démarches Carte Grise en ligne", ['responses'=> $user]);
-
+        $this->mailManager->sendEmail([$user->getEmail()], $template, "Bienvenu sur CG Officiel - Démarches Carte Grise en ligne", ['responses'=> $user]);
         return 'success';
     }
 }
