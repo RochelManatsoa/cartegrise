8c4986964feaeda9f0a8237cd3e598c62a2c3b00
Iary-git <holiary.andria@gmail.com>
2020-02-27 16:50:58 +0300

    toggle-content Doc_telelechargeable deplacer dans home.js

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -347,17 +347,19 @@ $( function() {
     $( "#formulaire_codePostal" ).combobox();
     $('#AutoCompleteCP').find('.select').prepend($('ul#ui-id-1'));
 } );
-
 //fixed
 if (window.matchMedia("(min-width: 1900px)").matches) {
     $('#right_formulaireFx').find('.form_demarche').append($('.demarche_block'));
 } else {
     $('#col_formulaireFx').find('.form_demarche').append($('.demarche_block'));
 }
-
-
 //aside_right
 $("#aside_right").sticky({
     topSpacing: 200, 
     bottomSpacing: 1100
+});
+
+//doc-telechargeable
+$(".js-toggle").on("click", function () {
+    $(this).parent().next(".toggle-content").toggle();
 });
